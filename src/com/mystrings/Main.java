23ccee1d9cb990d7d9590ab722ee80bfeb703e39
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-06-03 12:21:37 +0200

    Add files via upload
    Committing Main class.

---
+++ src/com/mystrings/Main.java
@@ -0,0 +1,36 @@
+/**
+ *  Java program to demonstrate String class.
+ */
+package com.mystrings;
+
+/**
+ *  Main class.
+ */
+public class Main {
+
+    // JVM entry point.
+    public static void main(String[] args) {
+
+        // Creating an initialized string instance.
+        String str = "I am a string";
+
+        //  Checking ifstring has character 't'.
+        boolean hasT = str.contains("t");
+
+        // Printing boolean value of hasT.
+        System.out.println(hasT); // Output: true
+
+        // Finding index of character 'a'.
+        int index = str.indexOf("a");
+
+        // Printing value of index.
+        System.out.println(index); // Output: 2
+
+        // Finding index of next character 'a'.
+        int index1 = str.indexOf("a", 3); // Output:
+
+        // Printing value of index1.
+        System.out.println(index1); // Output: 5
+
+    }
+}
